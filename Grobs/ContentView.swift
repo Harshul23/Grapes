775e6953bcf78ba64d3ff6deb4c8638919cddbb6
Harshul <vishuseervi825@gmail.com>
2025-12-04 17:49:23 +0530

    Initial Commit

---
+++ Grobs/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Grobs
+//
+//  Created by Harshul Seervi on 04/12/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Grobs/GrobsApp.swift
@@ -0,0 +1,17 @@
+//
+//  GrobsApp.swift
+//  Grobs
+//
+//  Created by Harshul Seervi on 04/12/25.
+//
+
+import SwiftUI
+
+@main
+struct GrobsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
